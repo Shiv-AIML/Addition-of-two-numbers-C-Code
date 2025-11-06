509eaf6ce91317f2753467434b54bd3d6f2b0b07
Shiv-AIML <shivwaghe15@gmail.com>
2025-11-07 00:04:44 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int a=5;
+    int b=4;
+    
+    printf("Enter the num a:");
+    scanf("%d",&a);
+    
+    printf("Enter the num b:");
+    scanf("%d",&b);
+    
+    printf("sum of num is %d",a+b);
+    scanf("%d",a+b,a,b);
+    return 0;
+    
+}
